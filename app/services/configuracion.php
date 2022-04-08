7326be859bc02efc8ca703cdfda4b90ceb0be2ae
Leonardo <ldz.software@gmail.com>
2022-04-08 10:00:26 -0300

    Restauro el EndPoint de Rubros

--- app/services/configuracion.php
+++ app/services/configuracion.php
@@ -5,8 +5,8 @@
 
 class Configuracion {
 	// Configuración de acceso a base de datos
-	public static $server = "localhost";
+	public static $server = "192.168.1.250";
 	public static $uid = "root";
-	public static $password = "";
+	public static $password = "imprimir";
 	public static $dbName = "db_derweb";
 }
