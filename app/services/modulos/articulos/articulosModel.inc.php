32abf92fd8e8a262aef077161f83f07846e5befb
leonardo <info@linfow.com.ar>
2022-05-26 10:54:54 -0300

    derweb_upgrades: Agrego validación de campos en API artículos.

--- app/services/modulos/articulos/articulosModel.inc.php
+++ app/services/modulos/articulos/articulosModel.inc.php
@@ -29,6 +29,7 @@ class ArticulosModel extends Model {
         $aRegistro = json_decode($registro, true);
         $aResult = array();
         $bd = new BDObject();
+        $procesar = false;
         try {
             $rubro_cod = $aRegistro["U_ONESL_RubroCod"];
             $subrubro_cod = $aRegistro["U_ONESL_SubRubroCod"];
@@ -40,29 +41,65 @@ class ArticulosModel extends Model {
             $existencia_stock = 0.00;
             $stock_minimo = 0.00;
 
-            $sql = "CALL sp_articulos_upgrade (
-                    xrubroCod,
-                    xsubrubroCod,
-                    xmarcaCod,
-                    xcodigo,
-                    xcodOriginal,
-                    xdescripcion,
-                    xalicuotaIVA,
-                    xexistencia,
-                    xstockMinimo)";
-            $this->setParameter($sql, "xrubroCod", $rubro_cod);
-            $this->setParameter($sql, "xsubrubroCod", $subrubro_cod);
-            $this->setParameter($sql, "xmarcaCod", $marca_cod);
-            $this->setParameter($sql, "xcodigo", $codigo);
-            $this->setParameter($sql, "xcodOriginal", $codigo_original);
-            $this->setParameter($sql, "xdescripcion", $descripcion);
-            $this->setParameter($sql, "xalicuotaIVA", $alicuota_iva);
-            $this->setParameter($sql, "xexistencia", $existencia_stock);
-            $this->setParameter($sql, "xstockMinimo", $stock_minimo);
-            $bd->execQuery($sql);
+            // Valido que los datos que se requieren estén cargados en el JSON.
+            $procesar = true;
+            if (esVacio($aRegistro["ItemCode"])) {
+                $procesar = false;
+                $aResult["result_code"] = "VALID_ERROR";
+                $aResult["result_mensaje"] = "Campo código de artículo está vacío";                
+            }
 
-            $aResult["result_code"] = "OK";
-            $aResult["result_mensaje"] = "Articulo actualizado satisfactoriamente";
+            if (esVacio($aRegistro["ItemName"])) {
+                $procesar = false;
+                $aResult["result_code"] = "VALID_ERROR";
+                $aResult["result_mensaje"] = "Campo descripción de artículo está vacío";                    
+            }
+
+            if (esVacio($rubro_cod)) {
+                $procesar = false;
+                $aResult["result_code"] = "VALID_ERROR";
+                $aResult["result_mensaje"] = "Campo código de rubro está vacío";                
+            }
+
+            if (esVacio($subrubro_cod)) {
+                $procesar = false;
+                $aResult["result_code"] = "VALID_ERROR";
+                $aResult["result_mensaje"] = "Campo código de subrubro está vacío";                
+            }
+
+            if (esVacio($marca_cod)) {
+                $procesar = false;
+                $aResult["result_code"] = "VALID_ERROR";
+                $aResult["result_mensaje"] = "Campo código de marca está vacío";                
+            }
+
+            // Si los datos están correctos, entonces, envío a procesar.
+            if ($procesar) {
+                $sql = "CALL sp_articulos_upgrade (
+                        xrubroCod,
+                        xsubrubroCod,
+                        xmarcaCod,
+                        xcodigo,
+                        xcodOriginal,
+                        xdescripcion,
+                        xalicuotaIVA,
+                        xexistencia,
+                        xstockMinimo)";
+                $this->setParameter($sql, "xrubroCod", $rubro_cod);
+                $this->setParameter($sql, "xsubrubroCod", $subrubro_cod);
+                $this->setParameter($sql, "xmarcaCod", $marca_cod);
+                $this->setParameter($sql, "xcodigo", $codigo);
+                $this->setParameter($sql, "xcodOriginal", $codigo_original);
+                $this->setParameter($sql, "xdescripcion", $descripcion);
+                $this->setParameter($sql, "xalicuotaIVA", $alicuota_iva);
+                $this->setParameter($sql, "xexistencia", $existencia_stock);
+                $this->setParameter($sql, "xstockMinimo", $stock_minimo);
+                $bd->execQuery($sql);
+
+                $aResult["result_code"] = "OK";
+                $aResult["result_mensaje"] = "Articulo actualizado satisfactoriamente";
+            }
+
         } catch (Exception $ex) {
             $aResult["result_code"] = "BD_ERROR";
             $aResult["result_mensaje"] = $ex->getMessage();
