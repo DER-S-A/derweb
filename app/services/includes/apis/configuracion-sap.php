07b833353e5ccdde54388f7baf2140a8a6a227a3
Leonardo <info@linfow.com.ar>
2022-09-07 16:47:30 -0300

    Configuracion SAP: Saco true por case-sensitive.

--- app/services/includes/apis/configuracion-sap.php
+++ app/services/includes/apis/configuracion-sap.php
@@ -4,8 +4,8 @@
  */
 
 // URLs para consumir APIs de SAP.
-define ("URL_LOGIN_ETL", "https://181.119.112.208:5444/dev/api/ETL/GetToken", true);
-define ("URL_ENVIAR_PEDIDO", "https://181.119.112.208:5444/dev/api/ETL/GetAndProcessNews", true); 
+define ("URL_LOGIN_ETL", "https://181.119.112.208:5444/dev/api/ETL/GetToken");
+define ("URL_ENVIAR_PEDIDO", "https://181.119.112.208:5444/dev/api/ETL/GetAndProcessNews"); 
 
 // Establece la configuración de logueo al ETL.
 define ("BODY_LOGIN_ETL", array(
@@ -13,6 +13,6 @@ define ("BODY_LOGIN_ETL", array(
     "password" => "1234"));
 
 // Parametros de conexión para enviar en el body de las APIs del ETL
-define ("CONNECTOR_CODE", "DercliWeb", true);
-define ("FUNCIONALITY_CODE", "SalesQuotation", true);
+define ("CONNECTOR_CODE", "DercliWeb");
+define ("FUNCIONALITY_CODE", "SalesQuotation");
 ?>
