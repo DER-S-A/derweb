4f3afec93e364fe2da354fb2946fa95821bb1a9b
Rodrigo <rodrigoursi@gmail.com>
2022-09-12 12:03:27 -0300

    Comento el codigo formas de envio del carrito

--- app/services/modulos/forma-envio/formas-enviosModel.inc.php
+++ app/services/modulos/forma-envio/formas-enviosModel.inc.php
@@ -12,58 +12,37 @@ class Formas_enviosModel extends Model {
      * @return array $result
      */
 
-    /*public function get($xfilter) {
+    public function get($xfilter) {
+        // Armado de la sentencia SQL.
+        $sql = "SELECT * FROM formas_envios ";
+        $this->setWhere($sql, $xfilter);
+        return $this->getQuery($sql); 
+    }
+
+    public function getFormasEnviosMiCarrito($xfilter, $xid) { ///$xid es la id de sucursal
         // Armado de la sentencia SQL.
         $sql = "SELECT * FROM formas_envios ";
         $this->setWhere($sql, $xfilter);
         $sql = $this->getQuery($sql);
-        
-        $sqlx = "SELECT id_formaenvio FROM sucursales WHERE id=2";
-        $idFormaEnvio = $this->getQuery($sqlx);
-        $sqlx = "SELECT * FROM formas_envios WHERE id=6";
+
+        // Hago un cruce de tabla para saber que que forma de envio es la principal para esa sucursal.
+        $sqlx = "SELECT formas_envios.* FROM sucursales
+        INNER JOIN formas_envios ON sucursales.id_formaenvio = formas_envios.id
+        WHERE sucursales.id = $xid";
         $predeterminado = $this->getQuery($sqlx);
 
+        // Aca armo un json nuevo para poner como indice 0 la forma de envio principal.
         $resultado = [$predeterminado[0]];
-        //$resultado[] = $sql[0];
 
+        // Aca recorro las formas de envio para agregarlasa al json que ya tiene la forma de envio principal.
+        // Asi q uso un if para cuando se repota la forma de envio no se cargue.
         foreach ($sql as &$valor) {
-            //$resultado[] = $valor;
             if($valor['id'] != $predeterminado[0]['id']) {
                 $resultado[] = $valor;
             }
         }
         
         return $resultado;
-        //return $predeterminado[0]['descripcion'];
-        //return $sql[0]['descripcion'];
-    }*/
-
-    public function getFormasEnviosMiCarrito($xfilter, $xid) {
-        // Armado de la sentencia SQL.
-        $sql = "SELECT * FROM formas_envios ";
-        $this->setWhere($sql, $xfilter);
-        $sql = $this->getQuery($sql);
-        
-        $sqlx = "SELECT id_formaenvio FROM sucursales WHERE id=$xid";
-        $idFormaEnvio = $this->getQuery($sqlx);
-        $idFormaEnvio = $idFormaEnvio[0]['id_formaenvio'];
-        $sqlx = "SELECT * FROM formas_envios WHERE id=$idFormaEnvio";
-        $predeterminado = $this->getQuery($sqlx);
-
-        $resultado = [$predeterminado[0]];
-        //$resultado[] = $sql[0];
-
-        foreach ($sql as &$valor) {
-            //$resultado[] = $valor;
-            if($valor['id'] != $predeterminado[0]['id']) {
-                $resultado[] = $valor;
-            }
-        }
-        
-        return $resultado;
-        //return $idFormaEnvio;
-        //return $predeterminado[0]['descripcion'];
-        //return $sql[0]['descripcion'];
     }
 }
 
