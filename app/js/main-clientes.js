b9cafddc2ad1e9ff9f98b2feb2601af395758b2a
Rodrigo <rodrigoursi@gmail.com>
2023-01-18 16:44:22 -0300

    Se arreglo el boton menu (flechita)

--- app/js/main-clientes.js
+++ app/js/main-clientes.js
@@ -309,9 +309,13 @@ objTxtValorBuscado.addEventListener("keypress",(e) => {
 function esconderHamburguesa() {
     let objHambur = document.getElementById("menu-options");
     let objBotonLista = document.getElementById("btnPushListaArticulo");
+    let objBotonMenu = document.querySelector("#botonMenu");
+    let objBtnPush =  document.querySelector("#btnPushMenu");
     objBotonLista.addEventListener ("click", () => {
         objHambur.style.display = "none";
-        console.log(document.querySelector(".fa-xmark"))
+        //objBotonMenu.classList.replace("fa-chevron-up", "fa-chevron-down");
+        //objBotonMenu.className = "fa-solid fa-chevron-down";
+        objBtnPush.innerHTML = '<i id="botonMenu" class="fa-solid fa-chevron-down"></i>';
         document.querySelector("#botonHambur").className = "fas fa-bars";
     })
 }
