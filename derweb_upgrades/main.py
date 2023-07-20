47c76b56a4f78c9b432a5d35024fe37a3c1b51bc
Bautista Bianculli <bbianculli2000@gmail.com>
2023-07-20 11:44:57 -0300

    Actualizacion: Stock Agregado

--- derweb_upgrades/main.py
+++ derweb_upgrades/main.py
@@ -3,17 +3,18 @@ import time
 
 start_time = time.perf_counter()
 oCatalogo = Catalogo()
-oCatalogo.updatePaises()
-oCatalogo.updateProvincias()
-oCatalogo.updateFormasEnvios()
-oCatalogo.updateRubros()
-oCatalogo.updateSubrubros()
-oCatalogo.updateMarcas()
-oCatalogo.updateArticulos()
-oCatalogo.updateListasDePrecios()
-oCatalogo.updateTeleVentas()
-oCatalogo.updateClientes()
-oCatalogo.updateSucursales()
+# oCatalogo.updatePaises()
+# oCatalogo.updateProvincias()
+# oCatalogo.updateFormasEnvios()
+# oCatalogo.updateRubros()
+# oCatalogo.updateSubrubros()
+# oCatalogo.updateMarcas()
+# oCatalogo.updateArticulos()
+oCatalogo.updateStock()
+# oCatalogo.updateListasDePrecios()
+# oCatalogo.updateTeleVentas()
+# oCatalogo.updateClientes()
+# oCatalogo.updateSucursales()
 
 
 
@@ -21,4 +22,3 @@ elapsed_time = time.perf_counter() - start_time
 minute = int(elapsed_time / 60)
 seconds = int(elapsed_time % 60)
 print(f"Tiempo duracion total: {minute}.{seconds} ")
-
