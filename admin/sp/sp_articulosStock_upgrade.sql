d98ddc1f1ab1f9c337cd47762b9c9131295ba3a9
Leonardo D. Zulli <info@linfow.com.ar>
2023-12-27 18:07:45 -0300

    SP Actualización Stock

---
+++ admin/sp/sp_articulosStock_upgrade.sql
@@ -0,0 +1,33 @@
+CREATE PROCEDURE `sp_articulosStock_upgrade`(
+	IN `ItemCode` varchar(50),
+	IN `Cantidad` int
+)
+BEGIN
+     DECLARE IdArticulo int;
+     DECLARE EXIT HANDLER FOR SQLEXCEPTION
+     ROLLBACK;
+
+  BEGIN
+     GET DIAGNOSTICS CONDITION 1
+     @sqlstate = RETURNED_SQLSTATE,
+     @errno = MYSQL_ERRNO,
+     @text = MESSAGE_TEXT;
+
+     SET @message = CONCAT('Error ', @errno, ': ', @text, ' - SQLSTATE: ', @sqlstate);
+     
+     INSERT INTO log_sp (nombre_sp, mensaje_error)
+       VALUES ('articulosStock_Upgrade', @message);
+  END;
+  START TRANSACTION;
+
+  SET IdArticulo = 0;
+
+  SELECT id INTO IdArticulo FROM articulos WHERE codigo = ItemCode;
+
+  IF IdArticulo != 0 THEN
+    UPDATE articulos SET existencia_stock = Cantidad WHERE id = IdArticulo;
+  END IF;
+
+  COMMIT;
+
+END
