3be41c71264da1b0738396d4b720a618be43f6f9
Leonardo D. Zulli <ldz.software@gmail.com>
2024-01-17 17:46:44 -0300

    Corrección de manejos de transacciones.

--- admin/sp/sp_artPrecios_upgrade.sql
+++ admin/sp/sp_artPrecios_upgrade.sql
@@ -1,6 +1,7 @@
-CREATE PROCEDURE sp_artPrecios_upgrade (IN ItemCode text,
-IN PriceList int,
-IN Price decimal(20,2))
+CREATE PROCEDURE sp_artPrecios_upgrade (
+  IN ItemCode text,
+  IN PriceList int,
+  IN Price decimal(20,2))
 BEGIN
     DECLARE vCantReg int;
     DECLARE vIdArticulo int;
@@ -8,38 +9,57 @@ BEGIN
 
     DECLARE EXIT HANDLER FOR SQLEXCEPTION
     BEGIN
-    GET DIAGNOSTICS CONDITION 1
-    @sqlstate = RETURNED_SQLSTATE,
-    @errno = MYSQL_ERRNO,
-    @text = MESSAGE_TEXT;
-    SET @message = CONCAT('Error ', @errno, ': ', @text);
-    INSERT INTO log_sp (nombre_sp, mensaje_error)
-      VALUES ('artPrecios_upgrade', @message);
+      ROLLBACK;
+      GET DIAGNOSTICS CONDITION 1
+        @sqlstate = RETURNED_SQLSTATE,
+        @errno = MYSQL_ERRNO,
+        @text = MESSAGE_TEXT;
+      SET @message = CONCAT('Error ', @errno, ': ', @text);
+      INSERT INTO log_sp (nombre_sp, mensaje_error)
+        VALUES ('artPrecios_upgrade', @message);
     END;
 
+    START TRANSACTION;
     SELECT
-    id INTO vIdArticulo
-    FROM articulos
-    WHERE codigo = ItemCode;
-    SELECT
-    id INTO @vIdPriceList
-    FROM listas_precios
-    WHERE price_list = PriceList;
+      id 
+    INTO 
+      vIdArticulo
+    FROM 
+      articulos
+    WHERE 
+      codigo = ItemCode;
 
     SELECT
-    COUNT(*) INTO vCantReg
-    FROM articulos_precios
-    WHERE id_articulo = vIdArticulo;
+      id 
+    INTO 
+      @vIdPriceList
+    FROM 
+      listas_precios
+    WHERE 
+      price_list = PriceList;
+
+    SELECT
+      COUNT(*) 
+    INTO 
+      vCantReg
+    FROM 
+      articulos_precios
+    WHERE 
+      id_articulo = vIdArticulo;
 
     IF vCantReg = 0 THEN
       INSERT INTO articulos_precios (id_listaprecio, id_articulo, precio_lista, precio_anterior)
         VALUES (@vIdPriceList, vIdArticulo, Price, 0);
     ELSE
       SELECT
-        COUNT(*) INTO vCantRegPrecios
-      FROM articulos_precios
-      WHERE id_articulo = vIdArticulo
-      AND id_listaprecio = @vIdPriceList;
+        COUNT(*) 
+      INTO 
+        vCantRegPrecios
+      FROM 
+        articulos_precios
+      WHERE 
+        id_articulo = vIdArticulo AND
+        id_listaprecio = @vIdPriceList;
 
       IF vCantRegPrecios = 0 THEN
 
@@ -49,10 +69,15 @@ BEGIN
       ELSEIF vCantRegPrecios = 1 THEN
 
         SELECT
-          precio_lista INTO @vPrecioAnterior
-        FROM articulos_precios
-        WHERE id_articulo = vIdArticulo
-        AND id_listaprecio = @vIdPriceList;
+          precio_lista 
+        INTO 
+          @vPrecioAnterior
+        FROM 
+          articulos_precios
+        WHERE 
+          id_articulo = vIdArticulo
+        AND 
+          id_listaprecio = @vIdPriceList;
 
         IF @vPrecioAnterior != Price THEN
           UPDATE articulos_precios
@@ -64,4 +89,5 @@ BEGIN
 
       END IF;
     END IF;
+    COMMIT;
 END
